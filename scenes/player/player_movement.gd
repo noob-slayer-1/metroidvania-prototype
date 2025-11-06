f7c91649dac504ce487b2d269bfef812ff655e76
noob-slayer-1 <Cinnamon>
2025-11-06 16:52:43 +0700

    feat: add double jump

--- scenes/player/player_movement.gd
+++ scenes/player/player_movement.gd
@@ -3,6 +3,7 @@ extends Node2D
 @export var jump_force: float = 350.0
 @export var air_control: float = 10.0
 var last_facing_direction = Vector2.RIGHT
+var double_jump = false
 @onready var player: CharacterBody2D = get_owner()
 
 
@@ -12,7 +13,10 @@ func move() -> void:
 		player.velocity.x = player.MOVE_SPEED * input_velocity.x
 		if input_velocity:
 			last_facing_direction = input_velocity.normalized()
-		if Input.is_action_just_pressed(&"jump"):
+		if Input.is_action_just_pressed(&"jump") and double_jump:
+			if not player.is_on_floor():
+				double_jump = false
+			
 			if input_velocity:
 				apply_force(Vector2.UP + (input_velocity * 0.8), jump_force)
 			else:
@@ -23,12 +27,14 @@ func move() -> void:
 		elif player.velocity.x < 0:
 			$"../Sprite2D".flip_h = true
 		
-		if player.is_on_floor() and player.velocity.x:
-			$"../AnimationPlayer".play(&"moving")
-		elif not player.is_on_floor():
+		if player.is_on_floor():
+			double_jump = true
+			if player.velocity.x:
+				$"../AnimationPlayer".play(&"moving")
+			else :
+				$"../AnimationPlayer".play(&"idle")
+		else:
 			$"../AnimationPlayer".play(&"jump")
-		else :
-			$"../AnimationPlayer".play(&"idle")
 
 func apply_force(direction: Vector2, force: float):
 	player.velocity = direction * force
