dfd609ce59339b7495bedd6600832288aeb2f094
noob-slayer-1 <Cinnamon>
2025-11-06 13:07:54 +0700

    refactor: unused code

--- scenes/player/player_movement.gd
+++ scenes/player/player_movement.gd
@@ -6,30 +6,9 @@ var last_facing_direction = Vector2.RIGHT
 @onready var player: CharacterBody2D = get_owner()
 
 
-func move(delta) -> void:
+func move() -> void:
 	if not player.is_in_knockback:
 		var input_velocity = Vector2(Input.get_axis(&"move_left", &"move_right"), 0)
-		#if player.is_on_floor():
-			#player.velocity.x = player.MOVE_SPEED * input_velocity.x
-			#if input_velocity:
-				#last_facing_direction = input_velocity.normalized()
-			#if Input.is_action_just_pressed(&"jump"):
-				#if input_velocity:
-					#apply_force(Vector2.UP + (input_velocity * 0.8), jump_force)
-				#else:
-					#apply_force(Vector2.UP, jump_force)
-			#if player.velocity.x:
-				#if player.velocity.x > 0:
-					#$"../Sprite2D".flip_h = false
-				#elif player.velocity.x < 0:
-					#$"../Sprite2D".flip_h = true
-				#$"../AnimationPlayer".play(&"moving")
-			#else :
-				#$"../AnimationPlayer".play(&"idle")
-		#else :
-			#var new_value = player.velocity.x + player.MOVE_SPEED * input_velocity.x * air_control * delta
-			#player.velocity.x = clamp(new_value, player.MOVE_SPEED * -1, player.MOVE_SPEED)
-			#$"../AnimationPlayer".play(&"jump")
 		player.velocity.x = player.MOVE_SPEED * input_velocity.x
 		if input_velocity:
 			last_facing_direction = input_velocity.normalized()
